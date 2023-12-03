072794650ff5b9974b5656133841482aa951cdfc
ihr-schurtz <152788173+ihr-schurtz@users.noreply.github.com>
2023-12-03 14:17:47 +0100

    Update schutz.js

--- api/schutz.js
+++ api/schutz.js
@@ -1,14 +1,11 @@
+import { redirect } from 'next/navigation';
+
 export default async function handler(req, res) {
     if (req.method === 'POST') {
         const data = req.body;
         console.log(req.body)
-            const filePath = path.join(process.cwd(), 'fertig.html');
-            
-            // Read the file
-            const htmlContent = await readFile(filePath, 'utf8');
 
-            // Send the HTML response
-            res.status(200).send(htmlContent);
+        redirect("/fertig.html");
     } else {
         res.status(405).send('Method Not Allowed');
     }
