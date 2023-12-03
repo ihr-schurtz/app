caaff6d9cc467476dc6b1dae793fdeaf045be779
ihr-schurtz <152788173+ihr-schurtz@users.noreply.github.com>
2023-12-03 14:25:41 +0100

    Update schutz.js

--- api/schutz.js
+++ api/schutz.js
@@ -2,9 +2,9 @@ export default async function handler(req, res) {
     if (req.method === 'POST') {
         const data = req.body;
         console.log(req.body)
-
-        res.redirect("/fertig.html")
     } else {
-        res.status(405).send('Method Not Allowed');
+        console.log(req)
     }
+    
+    res.redirect("/fertig.html")
 }
